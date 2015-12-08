106a52e70680f116f6d1ab0c5dd6f9cf0dba8bce
liangjfc <liangjfc@yonyou.com>
2015-12-08 10:46:31 +0800

    timestamp_to_date bugs fixed
    Change-Id: I430e41ece9c335a7bf28d868e24b784a7ce9a473
    Signed-off-by: liangjfc <liangjfc@yonyou.com>

--- function/timestamp_to_date.py
+++ function/timestamp_to_date.py
@@ -7,14 +7,23 @@ class function(function_base):
     def __init__(self):
         pass
     
-    
+    def check_params(self,params):
+        if len(params) == 0:
+            raise ValueError("Arguments error, no arguments added after function.")
+        if params[0] != "now":
+            if len(params[0]) != 10 and len(params[0]) !=13 :
+                raise ValueError("timestamp format error, its length should be 10 or 13. length: " + str(len(params[0])))
+                
+        
     @abc.abstractmethod
     def func_run(self,params):
         try:
             ts = params[0]
             if ts == "now":
                 ts = int(time.time())
-            time_array = time.localtime(ts)
+            elif len(ts) == 13:
+                ts = ts[0:10]
+            time_array = time.localtime(int(ts))
             date = time.strftime("%Y-%m-%d %H:%M:%S",time_array)
             print(date)
         except Exception as err:
